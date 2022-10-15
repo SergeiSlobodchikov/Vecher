7e878ae98b02ab35210c1f7c3c3bb26b0b8323db
Sergei Slobodchikov <cerhclobodhikow@mail.ru>
2022-10-15 14:57:26 +0300

    lekcia 2

--- praktika001/Program.cs
+++ praktika001/Program.cs
@@ -1,2 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// int[] a = {12, 242, 512, 53, 52, 632, 254, 242, 234};
+// int n = a.Length;
+// int find = 234;
+
+// int index= 0;
+// while(index < n)
+// {
+//     if(a[index] == find)
+//     {
+//         Console.WriteLine(index);
+//     }
+//     index++;
+// }
+void r(int[] col)
+{
+    int length = col.Length;
+    int index = 0;
+    while (index < length)
+    {
+        col[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+void p(int[] col2)
+{
+    int count = col2.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col2[position]);
+        position++;
+    }
+
+
+}
+
+int I(int[] col, int find)
+{
+    int count = col.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+     if (col[index] == find)
+     {
+        position = index;
+        break;
+     }
+     index++;   
+    }
+    return position;
+}
+
+int[] m = new int[15];
+r(m);
+p(m);
+Console.WriteLine();
+
+int pos = I(m, 4);
+Console.WriteLine(pos);
